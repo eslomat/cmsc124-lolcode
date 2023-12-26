f4aaa9b6db6dbb13f9f19503f0103251edb0357e
Kat Gonzales <125255901+klgonzales@users.noreply.github.com>
2023-12-26 21:37:11 +0800

    updated ui

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # _____________________________________________________________________________ IMPORT STATEMEMENTS
 
 import tkinter as tk
-from tkinter import filedialog, scrolledtext
+from tkinter import filedialog, scrolledtext, ttk
+
 from interpreter.lolcodeinterpreter import lolcodeinterpreter
 from interpreter.analyzer.debugger import print_code, print_lexemes_array, print_lexeme_dictionary
 from interpreter.analyzer.debugger import print_parse_tree, compare_tree_lexemes
@@ -16,36 +17,51 @@ class LOLCODEInterpreterGUI:
         # initialize file_path attribute
         self.file_path = None  
 
+        # load the png file
+        icon_file = "program-icon.png"
+        try:
+            icon_image = tk.PhotoImage(file=icon_file)
+            master.tk.call('wm', 'iconphoto', master._w, icon_image)
+        except tk.TclError:
+            # handle the case where the icon file couldn't be loaded
+            print(f"Could not load icon file: {icon_file}")
+
         # file explorer
-        self.file_explorer_label = tk.Label(master, text="File Explorer (1):")
+        self.file_explorer_label = tk.Label(master, text="File Explorer")
         self.file_explorer_label.grid(row=0, column=0)
         self.file_explorer_button = tk.Button(master, text="Select File", command=self.select_file)
         self.file_explorer_button.grid(row=1, column=0, pady=10)
 
         # text editor
-        self.text_editor_label = tk.Label(master, text="Text Editor (2):")
+        self.text_editor_label = tk.Label(master, text="Text Editor")
         self.text_editor_label.grid(row=0, column=1)
         self.text_editor = scrolledtext.ScrolledText(master, wrap=tk.WORD, width=40, height=10)
         self.text_editor.grid(row=1, column=1, pady=10)
 
-        # list of tokens
-        self.tokens_label = tk.Label(master, text="List of Tokens (3):")
+        # lexemes
+        self.tokens_label = tk.Label(master, text="Lexemes")
         self.tokens_label.grid(row=2, column=0, pady=10)
-        self.tokens_listbox = tk.Listbox(master, width=40, height=5)
-        self.tokens_listbox.grid(row=3, column=0, pady=10)
+        # create treeview widget for lexemes
+        self.tokens_tree = ttk.Treeview(master, columns=("Lexeme", "Classification"), show="headings", height=5)
+        self.tokens_tree.heading("Lexeme", text="Lexeme")
+        self.tokens_tree.heading("Classification", text="Classification")
+        self.tokens_tree.grid(row=3, column=0, pady=10)
 
         # symbol table
-        self.symbol_table_label = tk.Label(master, text="Symbol Table (4):")
+        self.symbol_table_label = tk.Label(master, text="Symbol Table")
         self.symbol_table_label.grid(row=2, column=1, pady=10)
-        self.symbol_table_listbox = tk.Listbox(master, width=40, height=5)
-        self.symbol_table_listbox.grid(row=3, column=1, pady=10)
+        # create treeview widget for symbol table
+        self.symbol_table_tree = ttk.Treeview(master, columns=("Identifier", "Value"), show="headings", height=5)
+        self.symbol_table_tree.heading("Identifier", text="Identifier")
+        self.symbol_table_tree.heading("Value", text="Value")
+        self.symbol_table_tree.grid(row=3, column=1, pady=10)
 
-        # execute/run Button
-        self.execute_button = tk.Button(master, text="Execute/Run (5)", command=self.run_code)
+        # execute/run button
+        self.execute_button = tk.Button(master, text="Execute/Run", command=self.run_code)
         self.execute_button.grid(row=4, column=0, columnspan=2, pady=10)
 
         # console
-        self.console_label = tk.Label(master, text="Console (6):")
+        self.console_label = tk.Label(master, text="Console")
         self.console_label.grid(row=5, column=0, pady=10)
         self.console_text = scrolledtext.ScrolledText(master, wrap=tk.WORD, width=100, height=5)
         self.console_text.grid(row=6, column=0, columnspan=2, pady=10)
@@ -59,29 +75,28 @@ class LOLCODEInterpreterGUI:
                 self.text_editor.insert(tk.END, code_content)
 
     def run_code(self):
-        # clear existing content in list of tokens, symbol table, and console
-        self.tokens_listbox.delete(0, tk.END)
-        self.symbol_table_listbox.delete(0, tk.END)
-        self.console_text.delete(1.0, tk.END)
-
         # check if a file has been selected
-        if self.file_path:  
+        if self.file_path:                  
             try:
-                # run LOLCODE interpreter with the selected file path
+                # run lolcode interpreter with the selected file path
                 lci = lolcodeinterpreter(self.file_path)
+                
+                # clear existing content in treeviews
+                self.tokens_tree.delete(*self.tokens_tree.get_children())
+                self.symbol_table_tree.delete(*self.symbol_table_tree.get_children())
 
-                # update symbol table
-                symbol_table = lci.get("symbol_table", {})
-                for identifier, value in symbol_table.items():
-                    self.symbol_table_listbox.insert(tk.END, f"{identifier} | {value}")
-
-                # update list of tokens
+                # update lexemes treeview
                 lexeme_dictionary = lci.get("lexeme_dictionary", {})
                 for lexeme, classification in lexeme_dictionary.items():
-                    self.tokens_listbox.insert(tk.END, f"{lexeme} | {classification}")
+                    self.tokens_tree.insert("", "end", values=(lexeme, classification))
+
+                # update symbol table treeview
+                symbol_table = lci.get("symbol_table", {})
+                for identifier, value in symbol_table.items():
+                    self.symbol_table_tree.insert("", "end", values=(identifier, value))
 
             except Exception as e:
-                # handle exceptions that might occur during LOLCODE interpretation
+                # handle exceptions that might occur during lolcode interpretation
                 error_message = f"Error during LOLCODE interpretation: {str(e)}"
                 self.console_text.insert(tk.END, error_message)
 
@@ -89,7 +104,7 @@ class LOLCODEInterpreterGUI:
                 # re-enable the "execute/run" button even if an error occurs
                 self.execute_button["state"] = tk.NORMAL
         else:
-            # inform the user that no file has been selected
+            # inform the user that no lolcode file has been selected
             self.console_text.insert(tk.END, "No LOLCODE file selected.")
 
 if __name__ == "__main__":
